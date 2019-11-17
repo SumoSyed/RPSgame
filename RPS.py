cd6f505d4b5ea15801b1e107f556289fbf6b82bd
Syed Mujahid Islam <syedmujahidislam@gmail.com>
2019-11-17 19:35:37 +0600

    Update RPS.py
    - Added a counter to a limit of 5 after which player will be asked if they would like to play or quit. 
    - Made changes to the computer's choice allowing it to be called with each round and thus allowing changes in computer's selection.

--- RPS.py
+++ RPS.py
@@ -1,39 +1,44 @@
 import random
 
-ai_choice = random.randint(1, 3)
+def ai_choice():
+    return random.randint(1, 3)
 
 Rock = 1
 Paper = 2
 Scissors = 3
+counter = 0
 
-def round(user_choice, ai_choice):
-
-    if ai_choice == Rock and user_choice == "r":
+def round(user_selection, ai_selection):
+    ai_selection = ai_choice()
+    if ai_selection == Rock and user_selection == "r":
         return "Draw"
-    elif ai_choice == Paper and user_choice == "r":
+    elif ai_selection == Paper and user_selection == "r":
         return "You lose. Paper beats rock!"
-    elif ai_choice == Scissors and user_choice == "r":
+    elif ai_selection == Scissors and user_selection == "r":
         return "You win. Rock beats scissors!"
 
-    elif ai_choice == Rock and user_choice == "p":
+    elif ai_selection == Rock and user_selection == "p":
         return "You win. Paper beats rock!"
-    elif ai_choice == Paper and user_choice == "p":
+    elif ai_selection == Paper and user_selection == "p":
         return "Draw."
-    elif ai_choice == Scissors and user_choice == "p":
+    elif ai_selection == Scissors and user_selection == "p":
         return "You lose. Scissors beats paper!"
 
-    elif ai_choice == Rock and user_choice == "s":
+    elif ai_selection == Rock and user_selection == "s":
         return "You lose. Rock beats scissors!"
-    elif ai_choice == Paper and user_choice == "s":
+    elif ai_selection == Paper and user_selection == "s":
         return "You win. Scissors beats paper!"
-    elif ai_choice == Scissors and user_choice == "s":
+    elif ai_selection == Scissors and user_selection == "s":
         return "Draw"
 
 
 while True:
     answer = input("Would you like to play Rock, Paper, Scissors? Type yes or no -> ").lower()
+    counter = 0
     if answer == 'yes':
-        print( round(input("Pick Rock, Paper, or Scissors. Type R or P or S: ").lower(), ai_choice))
+        while counter < 5:
+            print( round(input("Pick Rock, Paper, or Scissors. Type R or P or S: ").lower(), ai_choice))
+            counter = counter + 1
     elif answer == 'no':
         print("To play, restart the program. Press any key to exit")
         input()
